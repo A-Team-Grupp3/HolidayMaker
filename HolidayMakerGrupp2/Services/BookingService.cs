333e70f5c82c4c4c6f931ec4f38505628f234398
Antonnn95 <34644124+Antonnn95@users.noreply.github.com>
2021-05-06 15:57:08 +0200

    Påbörjat bookingService

--- HolidayMakerGrupp2/Services/BookingService.cs
+++ HolidayMakerGrupp2/Services/BookingService.cs
@@ -10,8 +10,64 @@ namespace HolidayMakerGrupp2.Services
     {
         private HolidayMakerGrupp2Context ctx = new HolidayMakerGrupp2Context();
 
-        public BookingService();
+        public BookingService()
+        {
 
+        }
+
+        public int CreateBooking(int customerId, DateTime arrival, DateTime departure, int accomodationsId, int transportationsId, int numberOfGuests, int nrKids, bool extraBed, int comfortId, double totPrice)
+        {
+            
+
+
+            var createdOrder = ctx.Bookings.Add(new Booking
+            {
+                CustomerId = customerId,
+                BookingDate = DateTime.Now,
+                DepartureDate = departure,
+                ArrivalDate = arrival,
+                TransportationId = transportationsId,
+                AccomodationsId = accomodationsId,
+                NrOfGuests = numberOfGuests,
+                NrOfKids = nrKids,
+                ExtraBed = extraBed,
+                ComfortId = comfortId,
+                TotalPrice = totPrice
+            });
+            ctx.SaveChanges();
+            return createdOrder.Entity.Id;
+        }
+
+        public void ChangeBooking(int id, Booking booking)
+        {
+            var oldBooking = ctx.Bookings.Find(id);
+
+            if (oldBooking.ArrivalDate != booking.ArrivalDate)
+            {
+                oldBooking.ArrivalDate = booking.ArrivalDate;
+            }
+            if (oldBooking.DepartureDate != booking.DepartureDate)
+            {
+                oldBooking.DepartureDate = booking.DepartureDate;
+            }
+            if (oldBooking.NrOfKids != booking.NrOfKids)
+            {
+                oldBooking.NrOfKids = booking.NrOfKids;
+            }
+            if (oldBooking.NrOfGuests != booking.NrOfGuests)
+            {
+                oldBooking.NrOfGuests = booking.NrOfGuests;
+            }
+            if (oldBooking.ExtraBed != booking.ExtraBed)
+            {
+                oldBooking.ExtraBed = booking.ExtraBed;
+            }
+            if (oldBooking.ComfortId != booking.ComfortId)
+            {
+                oldBooking.ComfortId = booking.ComfortId;
+            }
+            ctx.SaveChanges();
+        }
 
     }
 }
