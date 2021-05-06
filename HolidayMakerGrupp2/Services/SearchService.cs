6a2d065acf6c1edc7345c2d4945ac022cbd19ac3
ELIN-ORTEGAS-LE\elinw <elin.w.ortega@gmail.com>
2021-05-06 14:00:48 +0200

    Försöker lösa servicen för att söka på en stad och få upp alla boenden i den staden.

---
+++ HolidayMakerGrupp2/Services/SearchService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HolidayMakerGrupp2.Models.Database;
+
+namespace HolidayMakerGrupp2.Services
+{
+    public class SearchService
+    {
+        private readonly HolidayMakerGrupp2Context _context;
+
+        public SearchService(HolidayMakerGrupp2Context ctx)
+        {
+            _context = ctx;
+        }
+
+        public IEnumerable<Accomodation>SearchByCity(string city)
+        {
+            //IEnumerable<Accomodation> accomodation;
+            var cityId = _context.Cities.Where(c => c.Name.Contains(city));
+            List<City> result = cityId.ToList();
+            foreach  (var accomodation in result)
+            {
+                accomodation.Name = _context.Cities.Where(c => c.Name.Contains());
+            }
+            return result;
+        }
+    }
+}
