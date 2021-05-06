64ff8cedc562fe44f996b0a823bd7170c3f62154
CarlMM <kaptenkeps@gmail.com>
2021-05-06 11:36:46 +0200

    Skapat och Fyllt i CustomerService med metoder.

--- HolidayMakerGrupp2/Services/CustomerService.cs
+++ HolidayMakerGrupp2/Services/CustomerService.cs
@@ -8,8 +8,72 @@ namespace HolidayMakerGrupp2.Services
 {
     public class CustomerService
     {
-       // private HolidayMakerGrupp2 ctx = new HolidayMakerGrupp2();
+        private HolidayMakerGrupp2Context ctx = new HolidayMakerGrupp2Context();
+        
 
+
+        public CustomerService()
+        {
+
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            return ctx.Customers.ToList();
+        }
+
+        public IEnumerable<Customer> GetById(int id)
+        {
+            var customer = ctx.Customers.Where(c => c.Id == id);
+
+            return customer;
+        }
+
+        public IEnumerable<Customer> GetByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return ctx.Customers.ToList();
+            }
+            else
+            {
+                return ctx.Customers.Where(c => c.Firstname.ToLower().Contains(name)).ToList();
+            }
+        }
+
+        //Oklar enligt Carl
+        public string DeleteCustomer(int id)
+        {
+            var customerToDelete = ctx.Customers.Where(c => c.Id == id).Single<Customer>(); //Behövs single<Customer>?
+            ctx.Customers.Remove(customerToDelete);
+            ctx.SaveChanges();
+            return "Customer was succesfully Deleted"; //Ändras till något annat?
+        }
+
+        public int AddCustomer(string firstName, string lastName, string address, string city, string email, string phoneNr, int zipcode)
+        {
+            Guid g = Guid.NewGuid();
+
+
+            var createdCustomer = ctx.Customers.Add(new Customer
+            {
+
+
+                Firstname = firstName,
+                Lastname = lastName,
+                Address = address,
+                City = city,
+                Email = email,
+                PhoneNr = phoneNr,
+                Zipcode = zipcode,
+                GuId = g.ToString()
+
+
+
+            }) ;
+            ctx.SaveChanges();
+            return createdCustomer.Entity.Id;
+        }
 
     }
 }
--- HolidayMakerGrupp2/Services/TransportationService.cs
+++ HolidayMakerGrupp2/Services/TransportationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HolidayMakerGrupp2.Models.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace HolidayMakerGrupp2.Services
 {
     public class TransportationService
     {
+        private HolidayMakerGrupp2Context ctx = new HolidayMakerGrupp2Context();
+
+
+        public TransportationService()
+        {
+
+        }
+
     }
 }
