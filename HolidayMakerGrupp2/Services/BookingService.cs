bbd1c82e80ed61d5a63d5a1379e5969d8af807f5
CarlMM <kaptenkeps@gmail.com>
2021-05-06 15:32:51 +0200

    Skapade BookinServiceFil

---
+++ HolidayMakerGrupp2/Services/BookingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HolidayMakerGrupp2.Models.Database;
+
+namespace HolidayMakerGrupp2.Services
+{
+    public class BookingService
+    {
+        private HolidayMakerGrupp2Context ctx = new HolidayMakerGrupp2Context();
+
+        public BookingService();
+
+
+    }
+}
